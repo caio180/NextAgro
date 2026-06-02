8b43b6f97c0c76f859c709ab4bc364e2c85d9a8b
caio180 <caio.lima.viana2009@gmail.com>
2026-06-01 21:40:55 -0300

    Recebendo dados do front

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,17 @@ const porta = process.env.PORT
 app.use(cors())
 app.use(express.json())
 
+const sugestoes = []
+
 app.get('/', (req, res) => {
     res.send("Teste")
 })
+app.post('/formSugestao', (req, res) =>{
+    sugestoes.push(req.body)
+    res.json(sugestoes)
+})
+app.get('/forSugestao', (req, res) =>{
+    res.json(sugestoes)
+})
+
 app.listen(porta, ()=> console.log(`Servidor rodando: http://localhost:${porta}`))
