05d8aea9b8f16a8aaec63f5cac9c80a3e8814828
caio180 <caio.lima.viana2009@gmail.com>
2026-06-01 21:41:24 -0300

    Tentando enviar os dados pro back

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,49 @@
-console.log("Teste")
+const api = 'http://localhost:5000/formSugestao'
+function formuSuges(){
+    const form = document.getElementById('formSugestao')
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+
+        const nome = document.getElementById('formnome').value 
+        const email = document.getElementById('formemail').value 
+        const melhoria = document.getElementById('melhoria').value 
+        const assuntos = document.getElementById('assuntos').value 
+        const avaliacao = document.querySelector(
+            'input[name="avaliacao"]:checked'
+        )?.id;
+        fetch(api, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nome: nome,
+                    email: email,
+                    Sugestao_melhoria: melhoria,
+                    Sugestao_assuntos: assuntos,
+                    avaliacao: avaliacao
+
+                    
+                })
+            })
+            
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                alert("Cadastro realizado com sucesso");
+                 document.getElementById('formnome').value = "";
+                 document.getElementById('formemail').value = "";
+                 document.getElementById('melhoria'). value = "";
+                 document.getElementById('assuntos'). value = "";
+                 document
+        document.querySelectorAll('input[name="avaliacao"]').forEach(radio => {
+            radio.checked = false;
+        })
+            .catch(error => {
+            console.error(error);
+            alert("Erro ao enviar formulario");
+        });
+        
+        })
+    })
+}
