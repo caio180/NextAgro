65e7d778e2877a6e611336abd77ddd082a8a3f7c
caio180 <caio.lima.viana2009@gmail.com>
2026-06-06 15:21:31 -0300

    Tirando o || da variavel porta

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import rotas from './routes/rotas.js';
 const app = express();
-const porta = process.env.PORT || 5000;
+const porta = process.env.PORT
 
 app.use(cors());
 app.use(express.json());
